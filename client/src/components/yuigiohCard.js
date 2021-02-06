99f09166a084f42516de055e8ffab2d721af6400
kshaq777 <kshaq777@gmail.com>
2021-02-06 14:01:06 -0500

    initial card search functionality

---
+++ client/src/components/yuigiohCard.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+
+function YugiohCard(props) {
+  return (
+    <div key="props.id" className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+        {/* <li>
+            <strong>Images:</strong>
+            <select>
+            {props.sets.map && props.sets.map(set => {
+              return (<option>{set.set_name} | {set.set_rarity}</option>)
+            })}
+            </select>
+          </li> */}
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Type:</strong> {props.type}
+          </li>
+          <li>
+            <strong>Attack:</strong> {props.attack}
+          </li>
+          <li>
+            <strong>Defense:</strong> {props.defense}
+          </li>
+          <li>
+            <strong>Level:</strong> {props.level}
+          </li>
+  
+          <li>
+            <strong>Race:</strong> {props.race}
+          </li>
+          <li>
+            <strong>Attribute:</strong> {props.attribute}
+          </li>
+          <li>
+            <strong>Set:</strong>
+            <select>
+            {props.sets.map && props.sets.map(set => {
+              return (<option>{set.set_name} | {set.set_rarity}</option>)
+            })}
+            </select>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default YugiohCard;
