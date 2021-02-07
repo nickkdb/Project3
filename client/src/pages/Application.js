d7a97d8c92612e095d3739747bad40141f07dd78
Sam Scibetta <sammieeccentric@gmail.com>
2021-02-07 13:21:18 -0500

    started homepage and hero section

--- client/src/pages/Application.js
+++ client/src/pages/Application.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UserContext from "../utils/UserContext";
+import Nav from "../components/Nav"
 
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
@@ -9,33 +10,37 @@ import PasswordReset from "./PasswordReset";
 
 import Search from "./Search";
 import Dashboard from "./Dashboard"
+import Home from "./Home"
 
 function Application() {
   const user = useContext(UserContext);
 
   return (
-        user ?
-        <Router>
-          
+    user ?
+      <Router>
+        <Nav />
         <Switch>
           <Route exact path="/">
-                <ProfilePage />
+            <ProfilePage />
           </Route>
           <Route exact path="/search">
-                <Search />
+            <Search />
           </Route>
           <Route exact path="/dashboard">
-                <Dashboard />
+            <Dashboard />
           </Route>
         </Switch>
       </Router>
       :
       <Router>
+      <Route exact path="/">
+            <Home />
+          </Route>
         <Switch>
           <Route exact path="/signUp">
             <SignUp />
           </Route>
-          <Route exact path="/">
+          <Route exact path="/signIn">
             <SignIn />
           </Route>
           <Route exact path="/passwordReset">
