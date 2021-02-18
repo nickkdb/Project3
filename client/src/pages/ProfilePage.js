e301818db61deee5adb0602cb73e4e46c5422114
email <jjazminclrk@gmail.com>
2021-02-18 18:41:47 -0500

    nothing

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+// import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../utils/UserContext";
 import {auth} from "../utils/firebase";
 import API from "../utils/API";
