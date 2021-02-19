d4d3561be9e8f32bbba4858b0510e044dc5931e5
Sam Scibetta <sammieeccentric@gmail.com>
2021-02-18 22:09:04 -0500

    finished the profile banner

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import UserContext from "../utils/UserContext";
 import { auth } from "../utils/firebase";
 import API from "../utils/API";
 import MyCard from "../components/MyCard";
-import Banner from "../components/Banner";
+import ProfileBanner from "../components/ProfileBanner";
 import { Modal, Button, Form } from "react-bootstrap";
 
 const ProfilePage = () => {
@@ -55,35 +55,9 @@ const ProfilePage = () => {
 
   return (
     <div>
-      <Banner pageTitle={displayName} />
+      <ProfileBanner pageTitle={displayName} profileImg="https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png" email={email} userId={uid}/>
       <div className="container">
         <div className="mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
-          <div className="flex border flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
-            <div
-              style={{
-                background: `url(${photoURL ||
-                  "https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png"
-                  })  no-repeat center center`,
-                backgroundSize: "cover",
-                height: "200px",
-                width: "200px",
-              }}
-              className="border border-blue-300"
-            ></div>
-            <div className="md:pl-4">
-              <h2 className="text-2xl font-semibold">{displayName}</h2>
-              <h3 className="italic">{email}</h3>
-              <h3 className="italic">{uid}</h3>
-            </div>
-          </div>
-          <button
-            className="w-full py-3 bg-red-600 mt-4 text-white"
-            onClick={() => {
-              auth.signOut();
-            }}
-          >
-            Sign out
-      </button>
           {user &&
             user.mongo.products.map((card) => {
               return (
