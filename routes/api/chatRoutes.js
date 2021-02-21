2d3d41181f0012b491ebb95e67e6929cc70d7266
kshaq777 <kshaq777@gmail.com>
2021-02-21 16:25:28 -0500

    updated from main and commented out erroring code

--- routes/api/chatRoutes.js
+++ routes/api/chatRoutes.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const chatController = require("../../controllers/chatController");
 
 // Matches with "/api/books"
-router.route("/")
-.post(chatController.createRoom)
-  .get(chatController.checkForRoom)
+// router.route("/")
+// .post(chatController.createRoom)
+//   .get(chatController.checkForRoom)
   
 
-// Matches with "/api/books/:id"
-router.route("/:id")
-  .get(chatController.loadMessages)
-  .put(chatController.writeMessage)
+// // Matches with "/api/books/:id"
+// router.route("/:id")
+//   .get(chatController.loadMessages)
+//   .put(chatController.writeMessage)
 //   .delete(cardController.remove);
 
 module.exports = router;
