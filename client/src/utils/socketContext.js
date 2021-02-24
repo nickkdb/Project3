e0ec68b62f3009de8759febb0dd35c3346cd1cd8
wutango <nickkdb@gmail.com>
2021-02-23 22:01:34 -0500

    fixing bugs with chat models

--- client/src/utils/socketContext.js
+++ client/src/utils/socketContext.js
@@ -1,5 +1,5 @@
 import React from "react";
 import openSocket from 'socket.io-client';
 
-export const socket= openSocket('http://localhost:8080', {transports: ['websocket']});
+export const socket= openSocket('http://localhost:3001', {transports: ['websocket']});
 export const socketContext= React.createContext();
