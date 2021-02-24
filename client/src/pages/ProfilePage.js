661e8e71af3040d4ec52a2040f7b5c2277cfe556
kshaq777 <kshaq777@gmail.com>
2021-02-24 09:07:41 -0500

    re-add profile changes from front-end

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -168,11 +168,11 @@ const ProfilePage = () => {
         updatePicButton={updatePicButton}
       />
       <div className="container">
-        <div className="mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
+        <div className="row">
           {user &&
             user.mongo.products.map((card) => {
               return (
-                <div key={card.uuid}>
+                <div key={card.uuid} className="col-6">
                   <MyCard
                     key={card.uuid}
                     uuid={card.uuid}
