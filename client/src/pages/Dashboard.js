e9440fdd2e70b36f6fbe1012aedfe29f0c3244d5
Sam Scibetta <sammieeccentric@gmail.com>
2021-02-25 21:34:45 -0500

    ui changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,13 +3,14 @@ import UserContext from "../utils/UserContext";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { storage } from "../utils/firebase";
-import Banner from "../components/Banner";
+import ProfileBanner from "../components/ProfileBanner";
 import avatar from "../images/avatar.png";
 import Bar from "../components/Bar";
 import YourTrades from "../components/YourTrades";
 
 function Dashboard() {
   const user = useContext(UserContext);
+  const { displayName, email, uid } = user;
   const [search, setSearch] = useState("");
   const [list, setList] = useState([]);
   const [searchList, setSearchList] = useState([]);
@@ -68,123 +69,133 @@ function Dashboard() {
   useEffect(() => {
     API.getTrade(user.mongo.displayName)
       .then((res) => {
-        setYourTrades(res.data);      
+        setYourTrades(res.data);
       })
   }, []);
 
   function acceptTrade(id) {
-    
-    API.accept(id, {status: "accepted"})
-    .then((res) => window.location.reload());
-}
 
-function declineTrade(id) {
-  API.decline(id, {status: "declined"})
-  .then((res) => window.location.reload());
-}
+    API.accept(id, { status: "accepted" })
+      .then((res) => window.location.reload());
+  }
 
-function deleteTrade(id) {
-  API.delete(id)
-  .then((res) => window.location.reload());
-}
- 
+  function declineTrade(id) {
+    API.decline(id, { status: "declined" })
+      .then((res) => window.location.reload());
+  }
+
+  function deleteTrade(id) {
+    API.delete(id)
+      .then((res) => window.location.reload());
+  }
+
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-7">
-        <Bar  
-          trades={yourTrades}
-        />        
-          <div className= "col-2" />  
-          <h2> Your Trades </h2>
-        {yourTrades && yourTrades.map((trade) => {
-            let proposedBy= ""
-            let proposedTo= ""
-          console.log(trade.status)
-        trade.proposedByProducts[0].map((name) => {
-          (proposedBy === "" ? proposedBy += name.name : proposedBy += ", " + name.name)
-        })
+    <div>
+      <ProfileBanner
+        pageTitle={displayName}
+        avatar={avatar}
+        //fbImage={profilePic}
+        email={email}
+        userId={uid}
+        //updatePicButton={updatePicButton}
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col-7">
+            <Bar
+              trades={yourTrades}
+            />
+            <div className="col-2" />
+            <h2> Your Trades </h2>
+            {yourTrades && yourTrades.map((trade) => {
+              let proposedBy = ""
+              let proposedTo = ""
+              console.log(trade.status)
+              trade.proposedByProducts[0].map((name) => {
+                (proposedBy === "" ? proposedBy += name.name : proposedBy += ", " + name.name)
+              })
 
-        trade.proposedToProducts[0].map((name) => {
-          // proposedTo += name.name + " "
-          (proposedTo === "" ? proposedTo += name.name : proposedTo += ", " + name.name)
-        })
+              trade.proposedToProducts[0].map((name) => {
+                // proposedTo += name.name + " "
+                (proposedTo === "" ? proposedTo += name.name : proposedTo += ", " + name.name)
+              })
 
-        console.log(trade._id)
-          return (
-            <div className="col-8">
-            
-              <YourTrades
-                proposedBy={trade.proposedBy}
-                proposedTo={trade.proposedTo}
-                proposedByProducts={proposedBy}
-                proposedToProducts={proposedTo}
-                currentUser={user.mongo.displayName}
-                acceptTrade={acceptTrade}
-                declineTrade={declineTrade}
-                deleteTrade={deleteTrade}
-                status={trade.status}
-                id={trade._id}
-              >
-              </YourTrades>
-            </div>
-          )
-        })}
-        </div>
-        <div className="col-1">
+              console.log(trade._id)
+              return (
+                <div className="col-8">
 
-        </div>
-        <div className="col-4">
-          <form className="search">
-            <div className="form-group">
-              <label htmlFor="language">Search for values in any column:</label>
-              <input
-                value={search}
-                onChange={handleSearch}
-                name="term"
-                list="term"
-                type="text"
-                className="form-control"
-                placeholder="What are you looking for?"
-                id="term"
-              />
-              <small id="passwordHelpBlock" className="form-text text-muted">
-                * Search by displayName, card name, or card type. Results
-                include the username. Click to view profile and cards.
+                  <YourTrades
+                    proposedBy={trade.proposedBy}
+                    proposedTo={trade.proposedTo}
+                    proposedByProducts={proposedBy}
+                    proposedToProducts={proposedTo}
+                    currentUser={user.mongo.displayName}
+                    acceptTrade={acceptTrade}
+                    declineTrade={declineTrade}
+                    deleteTrade={deleteTrade}
+                    status={trade.status}
+                    id={trade._id}
+                  >
+                  </YourTrades>
+                </div>
+              )
+            })}
+          </div>
+          <div className="col-1">
+
+          </div>
+          <div className="col-4">
+            <form className="search">
+              <div className="form-group">
+                <label htmlFor="language">Search for values in any column:</label>
+                <input
+                  value={search}
+                  onChange={handleSearch}
+                  name="term"
+                  list="term"
+                  type="text"
+                  className="form-control"
+                  placeholder="What are you looking for?"
+                  id="term"
+                />
+                <small id="passwordHelpBlock" className="form-text text-muted">
+                  * Search by displayName, card name, or card type. Results
+                  include the username. Click to view profile and cards.
               </small>
-            </div>
-            <button
-              type="button"
-              onClick={filterSearch}
-              className="btn btn-primary ml-2 mb-4 mt-2"
-            >
-              Search
+              </div>
+              <button
+                type="button"
+                onClick={filterSearch}
+                className="btn btn-primary ml-2 mb-4 mt-2"
+              >
+                Search
             </button>
-            {searchList &&
-              searchList.map((item) => {
-                let link = `profile/${item.displayName}`;
-                return (
-                  <div className="row border">
-                    <div className="col">
-                      <img
-                        src={
-                          item.image ? item.image
-                            : avatar
-                        }
-                        alt={item.displayName + "Image"}
-                        style={{ width: "50%" }}
-                      ></img>
+              {searchList &&
+                searchList.map((item) => {
+                  let link = `profile/${item.displayName}`;
+                  return (
+                    <div className="row border">
+                      <div className="col">
+                        <img
+                          src={
+                            item.image ? item.image
+                              : avatar
+                          }
+                          alt={item.displayName + "Image"}
+                          style={{ width: "50%" }}
+                        ></img>
+                      </div>
+                      <div className="col">
+                        <p>
+                          <Link to={link}>{item.displayName}</Link>
+                        </p>
+                      </div>
                     </div>
-                    <div className="col">
-                      <p>
-                        <Link to={link}>{item.displayName}</Link>
-                      </p>
-                    </div>
-                  </div>
-                );
-              })}
-          </form>
+                  );
+                })}
+            </form>
+          </div>
         </div>
       </div>
     </div>
