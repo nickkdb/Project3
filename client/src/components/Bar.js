afd3ac3bfb5e12758295b4a31c767642c827dc71
kshaq777 <kshaq777@gmail.com>
2021-02-25 19:24:28 -0500

    stashing

--- client/src/components/Bar.js
+++ client/src/components/Bar.js
@@ -4,47 +4,49 @@ import {HorizontalBar} from 'react-chartjs-2';
 import UserContext from "../utils/UserContext";
 
 function Bar (props) {
-    const trades = props.trades
     const user = useContext(UserContext)
-    const [selectedTrade, setSelectedTrade] = useState({})
-    const [myPrices, setMyPrices] = useState(0)
-    const [yourPrices, setYourPrices] = useState(0)
+    const [selectedTrade, setSelectedTrade] = useState(props.trades[0])
+    const [byPrices, setByPrices] = useState(0)
+    const [toPrices, setToPrices] = useState(0)
+    const [chartState, setChartState] = useState({})
     if (selectedTrade){console.log(selectedTrade)}
 
     const cardNames = user.mongo.products.map(item => item.name)
     const cardPrices = user.mongo.products.map(item => item.price)
 
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;    
 
-    if (yourPrices){console.log(yourPrices.reduce(reducer))}
-    if (myPrices){console.log(myPrices.reduce(reducer))}
-    
     useEffect(() => {
-        setSelectedTrade(props.trades[0])
-      }, []);
+        if (selectedTrade) {
+            let tempBy = selectedTrade && selectedTrade.proposedByProducts.map(item => item.price).reduce(reducer)
+            let tempTo = selectedTrade && selectedTrade.proposedToProducts.map(item => item.price).reduce(reducer)
+            console.log(tempBy)
+            setByPrices(tempBy)
+            setToPrices(tempTo)
+            setTheChart()
+        }
+    }, [selectedTrade])
 
-    // useEffect(() => {
-    //     let tempM = selectedTrade && selectedTrade.proposedByProducts.map(item => item.price)
-    //     setMyPrices(tempM)
-    //     // setYourPrices(selectedTrade && selectedTrade.proposedToProducts.map(item => item.price))
-    // }, [selectedTrade])
-
-    const state = {
-        labels: cardNames,
-        datasets: [
-    
-            // have to map over all the cards a user has
-            {
-                label: "Your cards value",
-                backgroundColor: 'rgba(94, 118, 253, 0.2)',
-                borderColor: 'rgba(25, 59, 247, 0.2)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(0, 26, 172, 0.2)',
-                hoverBorderColor: 'rgb(54, 40, 255)',
-                data: cardPrices
-            },
-        ]
+    const setTheChart = () => {
+        let tempChart = {
+            labels: [selectedTrade.proposedBy, selectedTrade.proposedTo],
+            datasets: [
+        
+                // have to map over all the cards a user has
+                {
+                    label: "Your cards value",
+                    backgroundColor: 'rgba(94, 118, 253, 0.2)',
+                    borderColor: 'rgba(25, 59, 247, 0.2)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(0, 26, 172, 0.2)',
+                    hoverBorderColor: 'rgb(54, 40, 255)',
+                    data: [byPrices, toPrices]
+                },
+            ]
+        }
+        setChartState(tempChart)
     }
+
     const options = { 
         scales: { 
             xAxes: [{
@@ -59,7 +61,7 @@ function Bar (props) {
     return (
         <>
         <h2 style={{textAlign: "center"}}>Card Comparison</h2>
-        <HorizontalBar options={options} data={state} />
+        <HorizontalBar options={options} data={chartState} />
         </>
     )
 
