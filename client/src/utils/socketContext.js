a47aa31722e101865e93cfa3eaf65fd49c4e739c
wutango <nickkdb@gmail.com>
2021-03-01 21:11:52 -0500

    made socket URI env variable to account for local and deployed differences

--- client/src/utils/socketContext.js
+++ client/src/utils/socketContext.js
@@ -1,5 +1,5 @@
 import React from "react";
 import openSocket from 'socket.io-client';
 
-export const socket= openSocket('https://nerdherd-group5.herokuapp.com/', {transports: ['websocket']});
+export const socket= openSocket(process.env.REACT_APP_SOCKET_URI, {transports: ['websocket']});
 export const socketContext= React.createContext();
