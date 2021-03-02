784d558c49714e1ed9891b473a9b0200a775e590
wutango <nickkdb@gmail.com>
2021-03-01 22:47:07 -0500

    added click function to notification to route to messages page

--- client/src/pages/Application.js
+++ client/src/pages/Application.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { store } from 'react-notifications-component';
 import UserContext from "../utils/UserContext";
 import { socketContext } from "../utils/socketContext";
@@ -22,22 +22,35 @@ function Application() {
   const user = useContext(UserContext);
   const socket= useContext(socketContext);
 
+  const style= {
+    color: "white !important",
+    textDecoration: "underline"
+  }
+
   useEffect(() => {
     socket.on("notification", data => {
       if (data.receiver === user.displayName) {
-        store.addNotification({
-          title: "Alert",
-          message: `You have a new message from ${data.sender}`,
-          type: "warning",
-          insert: "top",
-          container: "top-right",
-          animationIn: ["animate__animated", "animate__fadeIn"],
-          animationOut: ["animate__animated", "animate__fadeOut"],
-          dismiss: {
-            duration: 3000,
-            onScreen: true
-          }
-        });
+          store.addNotification({
+            title: "Alert",
+            message: ( 
+              <>
+              <p>You have a new message from {data.sender}</p>
+              <a style={style} href="/messages">go there now</a>
+              </>
+             ),
+            type: "warning",
+            insert: "top",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+              duration: 5000,
+              onScreen: true,
+              pauseOnHover: true,
+              click: false,
+              showIcon: true
+            }
+          });
       }
     });
 
